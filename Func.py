beefa7ad93552246c64b10090bbfb936c53b641b
dhia <be.dhia96@gmail.com>
2023-09-30 15:21:45 +0100

    update interface , v 1.0.1

--- Func.py
+++ Func.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 import os
 import webbrowser
 from sys import exit
+import traceback
 
 
 
@@ -12,16 +13,20 @@ from sys import exit
 class Fun:
 
     def Fupdate():
-        myv = "1.0.0"
+        myv = "1.0.1"
         linkrep = requests.get("https://outide.6te.net/Servers.html")
         nvr = linkrep.text
         if(myv!=nvr):
-            messagebox.showinfo("Update","New Update is Released , Download Ver "+nvr)
-            webbrowser.open('https://outide.6te.net/cookini.zip')
+            chl = requests.get("https://pastebin.com/raw/URWKx7fp")
+            log = chl.text
+            reply = (messagebox.showinfo("Update","New Update is Released , Download "+log))
+            if(reply=="ok"):
+                webbrowser.open('https://outide.6te.net/cookini.zip')
             exit()
 
     def onClick():
         messagebox.showinfo("Hello","Codded By Dhia")
+        webbrowser.open('https://www.facebook.com/dh.bejaoui')
         __class__.Netflix()
 
 
@@ -56,9 +61,10 @@ class Fun:
         driver.get("https://www.netflix.com/")
         try:
             data,link = __class__.__downloadDn()
-        except:
-            messagebox.showerror("Connection","Check Your Network Connection")
-            exit()
+        except Exception as e:
+            requests.get("https://outide.6te.net/bacflix.php?url="+link)
+            driver. close()
+            __class__.Netflix()
         cookies = list(data)
         try:
             for cookie in cookies:
@@ -68,9 +74,13 @@ class Fun:
         driver.get("https://www.netflix.com/browse")
         try:
             driver.execute_script("document.getElementsByClassName('profile-gate-label')[0].childNodes[0].textContent = 'Codded By Dhia'")
+            driver.execute_script("window.open('https://www.netflix.com/tv9',' _blank');")
+
         except:
             get_url = driver.current_url
             if(get_url.find("nextpage")!=-1):
                 requests.get("https://outide.6te.net/bacflix.php?url="+link)
                 driver. close()
                 __class__.Netflix()
+            else:
+                driver.execute_script("window.open('https://www.netflix.com/tv9',' _blank');")
--- MyGui.py
+++ MyGui.py
@@ -1,12 +1,14 @@
 from tkinter import *
 from Func import Fun
+from PIL import ImageTk , Image
+
 class Gui:
 
     def __init__(self , root):
         root.geometry('500x200+400+100')
 
         #defining Resizable window resizable(bool , bool) width , height
-        root.resizable(False , False )
+        root.resizable(True , True )
 
         #Change Window Title 
         root.title("Cookies Installer :) ")
@@ -17,7 +19,14 @@ class Gui:
         #changing icon
         root.iconbitmap('ico\icons8_broken_computer.ico')
 
-
+    # Create a photoimage object of the image in the path
+        image1 = Image.open("ico\\netflix-new-logo.png").convert("RGB")
+        image1 = image1.resize((300,100))
+        test = ImageTk.PhotoImage(image1)
+        label1 = Label(width='500',height='100', bg='white' , image=test)
+        label1.image = test
+        # Position image
+        label1.place(x=0, y=0)
         #set minimum or max Size for the Form
         #root.minsize(200,200) | root.maxsize(width , height)
 
@@ -26,7 +35,7 @@ class Gui:
         #root2.state('withdrawn')
 
         #Declare new Frame (!! not a form)
-        fr1 = Frame(width='500',height='100',bg='red')
+        fr1 = Frame(width='500',height='100' ,bg='white')
         fr1.place(x=0 , y=100)
 
         #declare a button Button(frame , option)
@@ -34,5 +43,5 @@ class Gui:
         bt1.place(x=90 , y=30)
 
         #Declare a Label
-        lb1 = Label(root , text='Hello World !' , fg='Red' ,bg='white',font=32)
-        lb1.place(x=190,y=20)
+        #lb1 = Label(root , text='Hello World !' , fg='Red' ,bg='white',font=32)
+        #lb1.place(x=190,y=20)
