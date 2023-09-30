2296b4ed921b7e8a2878abc9d1c57d38d6c95cea
dhia <be.dhia96@gmail.com>
2023-09-30 20:41:16 +0100

    change some params to ver 1.0.2

--- Func.py
+++ Func.py
@@ -13,27 +13,33 @@ import traceback
 class Fun:
 
     def Fupdate():
-        myv = "1.0.1"
+        myv = "1.0.2"
         linkrep = requests.get("https://outide.6te.net/Servers.html")
         nvr = linkrep.text
         if(myv!=nvr):
             chl = requests.get("https://pastebin.com/raw/URWKx7fp")
             log = chl.text
+            ul = requests.get("https://pastebin.com/raw/0fG27qJx")
+            ult = ul.text
             reply = (messagebox.showinfo("Update","New Update is Released , Download "+log))
             if(reply=="ok"):
-                webbrowser.open('https://outide.6te.net/cookini.zip')
+                webbrowser.open(ult)
             exit()
 
     def onClick():
-        messagebox.showinfo("Hello","Codded By Dhia")
-        webbrowser.open('https://www.facebook.com/dh.bejaoui')
+        messagebox.showinfo("Codded By Dhia","You can follow Me on Insta @be__dhia")
         __class__.Netflix()
 
+    def contact():
+        webbrowser.open("https://www.facebook.com/dh.bejaoui")
 
     def __downloadDn():
         linkrep = requests.get("https://outide.6te.net/bacflix.php")
         link = linkrep.text
         print(link)
+        if(link==""):
+            messagebox.showerror("Error" , "Servers Are Currently Offline , Please Try Again Later")
+            exit()
         response = requests.get(link)
         data = response.text
         fh = open("config.dat","w")
@@ -84,3 +90,6 @@ class Fun:
                 __class__.Netflix()
             else:
                 driver.execute_script("window.open('https://www.netflix.com/tv9',' _blank');")
+
+    def shahid():
+        messagebox.showinfo("Next Version","We are currently Working to add shahid \n spotify \n HBO \n amazon \n ...")
--- MyGui.py
+++ MyGui.py
@@ -5,7 +5,7 @@ from PIL import ImageTk , Image
 class Gui:
 
     def __init__(self , root):
-        root.geometry('500x200+400+100')
+        root.geometry('500x600+400+100')
 
         #defining Resizable window resizable(bool , bool) width , height
         root.resizable(True , True )
@@ -27,6 +27,15 @@ class Gui:
         label1.image = test
         # Position image
         label1.place(x=0, y=0)
+
+        # Create a photoimage object of the image in the path
+        image1 = Image.open("ico\\shahid.png").convert("RGB")
+        image1 = image1.resize((300,200))
+        test = ImageTk.PhotoImage(image1)
+        label1 = Label(width='500',height='200', bg='white' , image=test)
+        label1.image = test
+        # Position image
+        label1.place(x=0, y=200)
         #set minimum or max Size for the Form
         #root.minsize(200,200) | root.maxsize(width , height)
 
@@ -41,6 +50,12 @@ class Gui:
         #declare a button Button(frame , option)
         bt1 = Button(fr1 , text='Netflix' , fg='Red' , bg='White',width='30',font='Arial',command=Fun.onClick)
         bt1.place(x=90 , y=30)
+
+        bt2 = Button(root , text='Shahid Vip' , fg='Green' , bg='White',width='30',font='Arial',command=Fun.shahid)
+        bt2.place(x=90 , y=400)
+
+        bt3 = Button(root , text='Contact Me' , fg='black' , bg='White',width='30',font='Arial',command=Fun.contact)
+        bt3.place(x=90 , y=500)
 
         #Declare a Label
         #lb1 = Label(root , text='Hello World !' , fg='Red' ,bg='white',font=32)
